3e070cf4043c368cd3fc3b738f2ce0ec91c9ed42
UchuNoKen <xen.o.fite@gmail.com>
2019-01-22 17:42:46 -0500

    Wrapped CommentDetail component with ApprovalCard component.

--- src/index.js
+++ src/index.js
@@ -2,40 +2,51 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45pm"
-        content="Nice post!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 12:00pm"
-        content="Keep up the good work!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 8:05am"
-        content="How the..."
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Will"
-        timeAgo="Yesterday at 11:55pm"
-        content="You rock!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Joe"
-        timeAgo="Yesterday at 7:31pm"
-        content="Who is here?"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45pm"
+          content="Nice post!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 12:00pm"
+          content="Keep up the good work!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 8:05am"
+          content="How the..."
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Will"
+          timeAgo="Yesterday at 11:55pm"
+          content="You rock!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Joe"
+          timeAgo="Yesterday at 7:31pm"
+          content="Who is here?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
